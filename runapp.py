c251617c828b634eab3f91f698f0fe8dd7eb372c
Tsahi Duek <tsahiduek@gmail.com>
2021-04-20 12:27:02 +0300

    logging

--- runapp.py
+++ runapp.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 from paste.deploy import loadapp
 from waitress import serve
@@ -6,5 +7,6 @@ from waitress import serve
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     app = loadapp('config:production.ini', relative_to='.')
+    logging.warning('just a simple warning message')
 
     serve(app, host='0.0.0.0', port=port)
